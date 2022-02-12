3559a090f43ab5ccdc385b5334b4b9397ee1df15
angelrosmend <angelrosmend@gmail.com>
2022-02-12 14:16:37 -0300

    set scss module config

---
+++ src/App.module.scss.d.ts
@@ -0,0 +1,5 @@
+export const appHeader: string;
+export const appLink: string;
+export const appLogoSpin: string;
+export const testScript: string;
+export const testScss: string;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss"
 
 function App() {
   return (
     <div className="App">
-     
+     <h1 className={styles.testScript}> Testing scss packages</h1>
     </div>
   );
 }
